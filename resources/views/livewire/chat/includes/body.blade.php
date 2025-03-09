bb547e8e622a7e63ca5525a33522def78017e2a7
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2025-03-09 16:24:47 +0100

    Correction de l'erreur liee a l'expression reguliere pour verifier les emojis

--- resources/views/livewire/chat/includes/body.blade.php
+++ resources/views/livewire/chat/includes/body.blade.php
@@ -98,8 +98,10 @@
                     $belongsToAuth = $message->belongsToAuth();
                     $parent = $message->parent ?? null;
                     $attachment = $message->attachment ?? null;
-                    $isEmoji = mb_ereg('^(?:\X(?=\p{Emoji}))*\X$', $message->body ?? '');
-
+                    // $isEmoji = preg_match('/[\x{1F600}-\x{1F64F}|\x{1F300}-\x{1F5FF}|\x{1F680}-\x{1F6FF}|\x{1F700}-\x{1F77F}|\x{1F780}-\x{1F7FF}|\x{1F800}-\x{1F8FF}|\x{1F900}-\x{1F9FF}|\x{1FA00}-\x{1FA6F}|\x{2600}-\x{26FF}|\x{2700}-\x{27BF}]/u', $message->body ?? '') === 1;
+                    // $isEmoji = mb_ereg('^(?:\X(?=\p{Emoji}))*\X$', $message->body ?? '');
+                    $isEmoji = false;
+                    
                     // keep track of previous message
                     // The ($key -1 ) will get the previous message from loaded
                     // messages since $key is directly linked to $message
